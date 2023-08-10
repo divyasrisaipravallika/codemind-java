d21ffe5483883aa3a65facd0a1dea1d92d71e26f
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-08-10 20:14:10 +0530

    import java.util.Scanner; public class can {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);             double a,b,c;             a = sc.nextDouble();             b = sc.nextDouble();             c = sc.nextDouble();             double s = (a+b+c)/2;             double ar = s*(s-a)*(s-b)*(s-c);             System.out.printf("%.2f",Math.sqrt(ar));      }
    }

---
+++ Area_of_Triangle_.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class can
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+            double a,b,c;
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            c = sc.nextDouble();
+            double s = (a+b+c)/2;
+            double ar = s*(s-a)*(s-b)*(s-c);
+            System.out.printf("%.2f",Math.sqrt(ar));
+     }
+
+}
