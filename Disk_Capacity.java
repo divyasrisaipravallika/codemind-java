124329819508e4bd43af13afa9f1a8c1dbb7be7d
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-08-12 11:59:10 +0530

    import java.util.Scanner; public class can {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);             int a;             a = sc.nextInt();             int b = sc.nextInt();             int c = sc.nextInt();             System.out.printf("%d",a*b*c*512*2);      }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class can
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+            int a;
+            a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.printf("%d",a*b*c*512*2);
+     }
+
+}
