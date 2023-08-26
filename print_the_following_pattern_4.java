b5639fad3e41b0d4cb78b4ce267a063ce735de41
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-08-26 11:13:13 +0530

    import java.util.Scanner; public class D {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         for(int i = 1 ; i <= n ; i++)         {             for(int j = 1 ; j <= n ; j++)             {                 if(i == j)                 {                     System.out.print("x");                 }                 else if((i+j)==n+1)                 {                     System.out.print("x");                 }                 else                 {                     System.out.print(0);                 }             }             System.out.println();         }     } }

---
+++ print_the_following_pattern_4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class D
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1 ; i <= n ; i++)
+        {
+            for(int j = 1 ; j <= n ; j++)
+            {
+                if(i == j)
+                {
+                    System.out.print("x");
+                }
+                else if((i+j)==n+1)
+                {
+                    System.out.print("x");
+                }
+                else
+                {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
